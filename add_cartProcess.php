be1a678321d8716669e8fe9e63232979cff231cf
SubhankarDRoy <subhankardebroy77@gmail.com>
2024-04-19 10:28:55 +0530

    Updated on 19-04-2024 at 10:28 IST

---
+++ add_cartProcess.php
@@ -0,0 +1,37 @@
+<?php
+
+    session_start();
+    $user=$_SESSION['username'];
+
+    require('connection.php');
+
+    $vid=$_GET['Vendor_ID'];
+    $med_name=$_GET['medname'];
+
+    
+    $query="Select * from cart where Vendor_ID='".$vid."';";
+    
+    $cart_list=mysqli_query($con,$query);
+    $arr=mysqli_fetch_array($cart_list,MYSQLI_NUM);
+    echo $arr[1];
+    if($vid!=$arr[1])
+    {
+        echo "
+            <script type=\"text/javascript\">
+            if(confirm(\"Your cart contains medicines from other vendor. do you want to clear it ?\")==true)
+            {
+                window.location.replace(\"clear_cartProcess.php\");
+            }
+            else
+            {
+                window.location.replace(\"med_list.php?Vendor_ID=$vid\");
+            }
+            </script>
+        ";
+    }
+    else
+    {
+        
+        $query="Select * from cart where Username=$user and med_name=$med_name";
+        $query="insert into cart value ($user,$vid,$med_name,1);";
+    }
