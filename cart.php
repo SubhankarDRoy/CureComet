4bff159a909e449ce33f3e937a328d51d2004442
SubhankarDRoy <subhankardebroy77@gmail.com>
2024-04-22 00:23:23 +0530

    Updaten on 22-04-2024 at 00:23 IST

--- cart.php
+++ cart.php
@@ -62,12 +62,12 @@
                         echo "</td>";
                         
                         echo "<td>";
-                            echo "<button onclick=\"document.location='adjust_qty.php?action=minus'\">-</button>";
+                            echo "<button onclick=\"document.location='adjust_cartQty.php?action=minus&vid=$arr[1]&mn=$arr[2]'\">-</button>";
                             echo "  $arr[3]  ";
-                            echo "<button onclick=\"document.location='adjust_qty.php?action=plus'\">+</button>";
+                            echo "<button onclick=\"document.location='adjust_cartQty.php?action=plus&vid=$arr[1]&mn=$arr[2]'\">+</button>";
                         echo "</td>";
                         echo "<td>";
-                            echo "<button class='delete-btn' onclick=\"document.location='adjust_qty.php?action=delete'\">Delete</button>";
+                            echo "<button class='delete-btn' onclick=\"document.location='adjust_cartQty.php?action=delete&vid=$arr[1]&mn=arr[2]'\">Delete</button>";
                         echo "</td>";
                     echo "</tr>";
                 }
