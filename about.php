ebedfec18dfd46ec5748d83b044cfbba194d81ba
SubhankarDRoy <subhankardebroy77@gmail.com>
2024-05-15 23:58:42 +0530

    Updated on 15-05-24 at 23:58 IST

--- about.php
+++ about.php
@@ -10,40 +10,47 @@
 </head>
 <body>
 <header>
-        <img class="logo" src="Images/curecomet_HQ.png" width="200px" height="150px" >
-        <nav>
-            <ul class="menu">
-                <li><a href="user_index.php">Home</a></li>
-                <li><a href="labs.php">Lab Tests</a></li>
-                <li><a href="appointment.php">Appointments</a></li>
-                <li><a href="counselling.php">Counselling</a></li>
-                <li><a href="cart.php">
-                    <?php
-                        session_start();
-                        $user=$_SESSION['username'];
-                        require('connection.php');
-                        $query="SELECT COUNT(*) FROM cart where Username ='$user'";
-                        $res=mysqli_query($con,$query);
-                        $arr=mysqli_fetch_array($res, MYSQLI_NUM);
-                        echo "Cart($arr[0])";
-                    ?>
-                </a></li>
-                <li><a href="account_detail.php">My Account</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="logout_process.php">Log Out</a></li>
-            </ul>
-        </nav>
-        
-        <?php
-            $name=$_SESSION['name'];
-            echo "<label id='header-label'> <b>Welcome</b> ".$name."</label>";
-        ?>
+    <center><img class="logo" src="Images/curecomet_HQ.png" width="200px" height="150px" ></center>
+    
     </header>
-    <div class="about us">
-    <img class="logo_about" src="Images/curecomet_HQ.png" width="200px" height="150px" >
-    <p class="about">CureComet is a pharmaceutical website that allows users to purchase medications from local pharmacies that have enrolled with CureComet. Apart from purchasing medicines, the users can also book doctor’s appointment or lab tests from their home. CureComet will have elements that will improve the user experience and comfort in addition to the ability to purchase medicines. CureComet targets to connect users with local pharmacies and clinics so that the process of medicine delivery is fast and affordable. The doctors with various specialties can enroll themselves and the users can book appointment with the doctors. It also allows user to connect with mental counselors all over the country for easy and secretive counseling sessions.
-        It will also help the user as well as the pharmacies to keep records of purchase and sales respectively. All the information can be viewed in one platform. The clinical records of a user can be monitored for future treatment purposes.</p>
-        
+    <br><br><br><br>
+    <div class="container">
+        <p class="paragraph">CureComet is a pharmaceutical website that allows users to purchase medications from local pharmacies that have enrolled with CureComet. Apart from purchasing medicines, the users can also book doctor’s appointment or lab tests from their home. CureComet will have elements that will improve the user experience and comfort in addition to the ability to purchase medicines. CureComet targets to connect users with local pharmacies and clinics so that the process of medicine delivery is fast and affordable. The doctors with various specialties can enroll themselves and the users can book appointment with the doctors. It also allows user to connect with mental counselors all over the country for easy and secretive counseling sessions.
+        It will also help the user as well as the pharmacies to keep records of purchase and sales respectively. All the information can be viewed in one platform. The clinical records of a user can be monitored for future treatment purposes.
+        </p>
     </div>
+
+    <table>
+        <tr>
+            <td><img  src="Images/sdr.jpg" width="200px" height="250px" ></td>
+            <td>
+                <b>About the developer</b><br><br>
+                <p>Hi! I am Subhankar Debroy. I am a computer enthusiast and love computer. You can contact me through the links given below.</p>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+            <a target="_blank" href="https://www.instagram.com/its._.sdr?igsh=MW94OTAzYWViNml5eQ=="><img  src="Images/insta.svg" width="40px" height="40px" ></a>&emsp;&emsp;
+            <a target="_blank" href="https://www.linkedin.com/in/subhankar-deb-roy-544429218?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img  src="Images/linkedin.svg" width="40px" height="40px" ></a>&emsp;&emsp;
+            <a target="_blank" href="#"><img  src="Images/gmail.svg" width="40px" height="40px" ></a>&emsp;&emsp;
+            <a target="_blank" href="https://x.com/SDR_Music01?t=KCzfOVOVYGQGlgj-hFHiUw&s=08"><img  src="Images/x.svg" width="40px" height="40px" ></a>&emsp;&emsp;
+            <a target="_blank" href="https://www.facebook.com/subhankar.debroy.50?mibextid=ZbWKwL"><img src="Images/fb.svg" width="40px" height="40px" ></a>&emsp;&emsp;
+            </td>
+            
+        </tr>
+    </table>
+
+    <br><br><br><br><br><br><br><br>
+    <footer id="footer">
+        &copy;<br>
+        Academic Project for BCA final semester<br>
+        developed by Subhankar Deb Roy (2021-24)<br>
+        Department of Computer Application, St. Edmund's College
+    </footer>
+
+</body>
+</html>
+    
 </body>
 </html>
